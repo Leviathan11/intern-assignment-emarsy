75adf5bb17bf43fbf797e87bdd2675835d88cf2d
Leviathan <gubik.levente@gmail.com>
2017-03-20 12:24:55 +0100

    2 additional edge case tests implemented

--- tour_manager_1/src/test/java/LogicTest.java
+++ tour_manager_1/src/test/java/LogicTest.java
@@ -3,6 +3,7 @@
  */
 import org.junit.*;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -74,5 +75,26 @@ public class LogicTest {
         assertTrue(returned.indexOf(w) > returned.indexOf(z));
         assertTrue(returned.indexOf(x) > returned.indexOf(u));
         assertTrue(returned.indexOf(y) > returned.indexOf(v));
+    }
+
+    @Test
+    public void emptyListTest(){
+        testTour = new Tour("Super Test Tour", new ArrayList<Location>());
+        assertEquals(0, testTour.calculateSequence().size());
+    }
+
+    @Test
+    public void locationDependencyMissingTest(){
+        Location u = new Location("U");
+        Location x = new Location("X", u);
+        Location z = new Location("Z");
+        Location w = new Location("W", z);
+        testTour = new Tour("Super Test Tour", Arrays.asList(u, w, x));
+        List<Location> returned = testTour.calculateSequence();
+        assertEquals(3, returned.size());
+        for (Location location: Arrays.asList(u, w, x)){
+            assertTrue(returned.contains(location));
+        }
+        assertTrue(returned.indexOf(x) > returned.indexOf(u));
     }
 }
